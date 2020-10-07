65714eeff5156495ce0bc50e1d6638bdea056d43
Mariano Lopez <marianolop22@yahoo.com.ar>
2020-10-07 12:37:13 -0300

    limpieza de proyecto

---
+++ src/HooksApp.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const HooksApp = props => {
+    return (
+        <div>
+            <h1>Hola Mundo</h1>
+        </div>
+    )
+}
+
+HooksApp.propTypes = {
+
+}
